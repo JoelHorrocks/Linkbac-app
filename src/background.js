f64b416b4b1d4f8907053a38e2db9e13723e0977
Joel Horrocks <joel.horrocks@gmail.com>
2023-04-02 12:07:08 +0200

    Added updating tasks when they change

--- src/background.js
+++ src/background.js
@@ -1,105 +1,192 @@
 'use strict';
 
-import { Client } from "@notionhq/client";
+import {Client} from "@notionhq/client";
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
+// With background scripts you can communicate with popup and contentScript
+// files. For more information on background script, See
+// https://developer.chrome.com/extensions/background_pages
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'NOTION') {
-    chrome.storage.sync.get({
-      authToken: "",
-      botId: "",
-      databaseId: "",
-      databaseTemplateId: "",
-      useNotionTemplate: true
-    }, function (items) {
-      addToNotion(request.payload.message, items);
-    })
-  }
-  // Bulk add tasks to Notion
-  else if (request.type === 'BULKNOTION') {
-    chrome.storage.sync.get({
-      authToken: "",
-      botId: "",
-      databaseId: "",
-      databaseTemplateId: "",
-      useNotionTemplate: true
-    }, function (items) {
-      for (let i = 0; i < request.payload.message.length; i++) {
-        // Spread out the requests to avoid rate limiting
-        setTimeout(() => {
-          addToNotion(request.payload.message[i], items);
-        }, 1500 * i);
-      }
-    })
-  }
-  else if(request.type === 'LOGGEDIN') {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.remove(tabs[0].id, function() { });
+chrome
+    .runtime
+    .onMessage
+    .addListener((request, sender, sendResponse) => {
+        if (request.type === 'NOTION') {
+            chrome
+                .storage
+                .sync
+                .get({
+                    authToken: "",
+                    botId: "",
+                    databaseId: "",
+                    databaseTemplateId: "",
+                    useNotionTemplate: true
+                }, function (items) {
+                    addToNotion(request.payload.message, items);
+                } // Bulk add tasks to Notion
+                )
+        } else if (request.type === 'BULKNOTION') {
+            chrome
+                .storage
+                .sync
+                .get({
+                    authToken: "",
+                    botId: "",
+                    databaseId: "",
+                    databaseTemplateId: "",
+                    useNotionTemplate: true
+                }, function (items) {
+                    for (let i = 0; i < request.payload.message.length; i++) {
+                        // Spread out the requests to avoid rate limiting
+                        setTimeout(() => {
+                            addToNotion(request.payload.message[i], items);
+                        }, 1500 * i);
+                    }
+                })
+        } else if (request.type === 'LOGGEDIN') {
+            chrome
+                .tabs
+                .query({
+                    active: true,
+                    currentWindow: true
+                }, function (tabs) {
+                    chrome
+                        .tabs
+                        .remove(tabs[0].id, function () {});
+                });
+            chrome
+                .tabs
+                .create({url: "onboarding.html?screen=screen-2"});
+        }
+        return true;
     });
-    chrome.tabs.create({url: "onboarding.html?screen=screen-2"});
-  }
-  return true;
-});
 
 async function addToNotion(message, items) {
-  const notion = new Client({ auth: items.authToken })
+    const notion = new Client({auth: items.authToken})
 
-  let databaseId = "";
-  // items.useNotionTemplate stores whether custom layout or built-in
-  if (items.databaseTemplateId != "" && items.databaseTemplateId != null) {
-    databaseId = items.databaseTemplateId;
-  }
-  else {
-    databaseId = items.databaseId;
-  }
+    let databaseId = "";
+    // items.useNotionTemplate stores whether custom layout or built-in
+    if (items.databaseTemplateId != "" && items.databaseTemplateId != null) {
+        databaseId = items.databaseTemplateId;
+    } else {
+        databaseId = items.databaseId;
+    }
 
-  try {
-    const response = await notion.pages.create({
-      parent: { database_id: databaseId },
-      properties: {
-        title: {
-          title: [
-            {
-              "text": {
-                "content": message["title"]
-              }
+    // Update task if a task with its ID is already in the Notion database
+    if (message["id"] != null) {
+        try {
+            const queryResponse = await notion
+                .databases
+                .query({
+                    database_id: databaseId,
+                    filter: {
+                        property: "ID",
+                        number: {
+                            equals: parseInt(message["id"])
+                        }
+                    }
+                })
+
+            try {
+                if (queryResponse.results.length === 0) {
+                    const addResponse = await notion
+                        .pages
+                        .create({
+                            parent: {
+                                database_id: databaseId
+                            },
+                            properties: {
+                                title: {
+                                    title: [
+                                        {
+                                            "text": {
+                                                "content": message["title"]
+                                            }
+                                        }
+                                    ]
+                                },
+                                "Type": {
+                                    select: {
+                                        name: message["type"]
+                                    }
+                                },
+                                "Class": {
+                                    select: {
+                                        name: message["class"]
+                                    }
+                                },
+                                "Criteria": {
+                                    multi_select: message["criteria"]
+                                },
+                                "Due": {
+                                    date: {
+                                        start: message["date"]
+                                    }
+                                },
+                                "ID": {
+                                    number: parseInt(message["id"])
+                                }
+                            },
+                            icon: {
+                                type: "emoji",
+                                emoji: message["emoji"]
+                            }
+                        })
+                    console.log(addResponse)
+                    console.log("Success! Entry added.")
+                } else {
+                    // Use fetch to update the page instead of the Notion API client because the
+                    // client page updating function does not work correctly (returns 400)
+
+                    const pageId = queryResponse.results[0].id;
+
+                    const requestOptions = {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Notion-Version': '2022-02-22',
+                            'Authorization': `Bearer ${items.authToken}`
+                        },
+                        body: JSON.stringify({
+                            "properties": {
+                                "title": {
+                                    "title": [
+                                        {
+                                            "text": {
+                                                "content": message["title"]
+                                            }
+                                        }
+                                    ]
+                                },
+                                "Type": {
+                                    "select": {
+                                        "name": message["type"]
+                                    }
+                                },
+                                "Criteria": {
+                                    "multi_select": message["criteria"]
+                                },
+                                "Due": {
+                                    "date": {
+                                        "start": message["date"]
+                                    }
+                                }
+                            }
+                        })
+                    };
+
+                    const response = await fetch(`https://api.notion.com/v1/pages/${pageId}`, requestOptions);
+                    if (response.status != 200) {
+                        console.error("Error updating page: " + response.status);
+                    } else {
+                        console.log(updateResponse)
+                        console.log("Success! Entry updated.")
+                    }
+                }
+            } catch (error) {
+                console.error(error.body)
             }
-          ]
-        },
-        "Type": {
-          select: {
-            name: message["type"]
-          }
-        },
-        "Class": {
-          select: {
-            name: message["class"]
-          }
-        },
-        "Criteria": {
-          multi_select: message["criteria"]
-        },
-        "Due": {
-          date: {
-            start: message["date"]
-          }
-        },
-        "ID": {
-          number: parseInt(message["id"])
+        } catch (error) {
+            console.error(error.body)
         }
-      },
-      icon: {
-        type: "emoji",
-        emoji: message["emoji"]
-      }
-    })
-    console.log(response)
-    console.log("Success! Entry added.")
-  } catch (error) {
-    console.error(error.body)
-  }
+    }
 }
--- src/popup.js
+++ src/popup.js
@@ -3,46 +3,69 @@
 import './popup.css';
 
 (function () {
-  function setupPage() {
-    document.getElementById('notion').addEventListener('click', () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const tab = tabs[0];
-      chrome.tabs.sendMessage(
-        tab.id,
-        {
-          type: 'NOTION',
-          payload: {},
-        },
-        (response) => {
-          console.log('Sent signal to contentScript to add to notion');
-        }
-      );
-      });
-    });
-    document.getElementById('settings').addEventListener('click', function() {
-      if (chrome.runtime.openOptionsPage) {
-        chrome.runtime.openOptionsPage();
-      } else {
-        window.open(chrome.runtime.getURL('options.html'));
-      }
-    });
-      // Check if the page is a task, the task overview page or another ManageBac page that does not support expoting by requesting from the content script
+    function setupPage() {
+        document
+            .getElementById('notion')
+            .addEventListener('click', () => {
+                chrome
+                    .tabs
+                    .query({
+                        active: true,
+                        currentWindow: true
+                    }, (tabs) => {
+                        const tab = tabs[0];
+                        chrome
+                            .tabs
+                            .sendMessage(tab.id, {
+                                type: 'NOTION',
+                                payload: {}
+                            }, (response) => {
+                                console.log('Sent signal to contentScript to add to notion');
+                            });
+                    });
+            });
+        document
+            .getElementById('settings')
+            .addEventListener('click', function () {
+                if (chrome.runtime.openOptionsPage) {
+                    chrome
+                        .runtime
+                        .openOptionsPage();
+                } else {
+                    window.open(chrome.runtime.getURL('options.html'));
+                }
+            });
+        // Check if the page is a task, the task overview page or another ManageBac page
+        // that does not support expoting by requesting from the content script
 
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tab = tabs[0];
-      if(tab.url.includes("core_tasks")) {
-        document.getElementById('notion').style.display = "block";
-      }
-      else if(tab.url.includes("tasks_and_deadlines")) {
-        document.getElementById('notion').style.display = "block";
-        document.getElementById('notion').innerText = "Add upcoming tasks to Notion";
-      }
-      else {
-        document.getElementById('notion').style.display = "none";
-      }
-    });
-  }
+        chrome
+            .tabs
+            .query({
+                active: true,
+                currentWindow: true
+            }, (tabs) => {
+                const tab = tabs[0];
+                if (tab.url.includes("core_tasks")) {
+                    document
+                        .getElementById('notion')
+                        .style
+                        .display = "block";
+                } else if (tab.url.includes("tasks_and_deadlines")) {
+                    document
+                        .getElementById('notion')
+                        .style
+                        .display = "block";
+                    document
+                        .getElementById('notion')
+                        .innerText = "Sync upcoming tasks with Notion";
+                } else {
+                    document
+                        .getElementById('notion')
+                        .style
+                        .display = "none";
+                }
+            });
+    }
 
-
-  document.addEventListener('DOMContentLoaded', setupPage);
+    document.addEventListener('DOMContentLoaded', setupPage);
 })();
